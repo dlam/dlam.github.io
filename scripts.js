053de82a4f88d25321315ea07dcbb7e4584a580c
Dustin Lam <mynameisdustinlam@gmail.com>
2015-08-01 16:02:19 -0700

    Add social media links.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 window.onload = function() {
-  var navBar = document.getElementById('navBar');
+  var navBar = document.querySelector('.navBar');
 
   navBar.onmouseenter = function(event) {
     this.style.width = '200px';
   };
 
   navBar.onmouseleave = function(event) {
-    this.style.width = '50px';
+    this.style.width = '75px';
   }
 }
