21c68781161e2f115226e1f6d1bb43bd2eba453f
Dustin Lam <mynameisdustinlam@gmail.com>
2015-08-08 01:59:23 -0700

    Move hover CSS styles out of JavaScript.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,1 @@
-window.onload = function() {
-  var navBar = document.querySelector('.navBar');
-
-  navBar.onmouseenter = function(event) {
-    this.style.width = '200px';
-  };
-
-  navBar.onmouseleave = function(event) {
-    this.style.width = '70px';
-  }
-}
+window.onload = function() {}
