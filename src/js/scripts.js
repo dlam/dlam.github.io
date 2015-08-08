d2c79086c50b85f9c729aa701f760a56620d1e0b
Dustin Lam <mynameisdustinlam@gmail.com>
2015-08-08 14:00:40 -0700

    Dynamically load articles from json file.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,26 @@
-window.onload = function() {}
+window.onload = function() {
+  var mainContainer = document.querySelector('.mainContainer');
+
+  var xmlHttp = new XMLHttpRequest();
+  var url = 'res/docs/blog/content.json';
+
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      var content = JSON.parse(xmlHttp.responseText);
+      for (var i = 0; i < content.length; i++) {
+        var articleHeader =
+          '<div class="articleContainer">' +
+          '<div class="article">';
+        var articleContent = content[i].title;
+        var articleFooter =
+          '</div>' +
+          '</div>';
+
+        mainContainer.innerHTML += articleHeader + articleContent + articleFooter;
+      }
+    }
+  }
+
+  xmlHttp.open("GET", url, true);
+  xmlHttp.send();
+}
