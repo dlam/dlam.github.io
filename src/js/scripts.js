19de98007034fc99d963f59e8de7aca2f8ad7f5e
Dustin Lam <mynameisdustinlam@gmail.com>
2015-08-08 21:26:02 -0700

    Setup to load article content dynamically.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,17 +8,23 @@ window.onload = function() {
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
       var content = JSON.parse(xmlHttp.responseText);
       for (var i = 0; i < content.length; i++) {
-        var articleHeader =
+        var articleImg = 'res/img/sample.jpg';
+        var articleTitle = content[i].title;
+        var articleContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+        mainContainer.innerHTML +=
           '<div class="articleContainer">' +
           '<div class="article">' +
-          '<img src="res/img/sample.jpg">';
-        var articleContent = content[i].title;
-        var articleFooter =
-          '</img>' +
+          '<img src="' +
+          articleImg +
+          '"></img>' +
+          '<div class="articleTitle">' +
+          articleTitle +
+          '</div>' +
+          '<div class="articleContent">' +
+          articleContent +
           '</div>' +
           '</div>';
-
-        mainContainer.innerHTML += articleHeader + articleContent + articleFooter;
       }
     }
   }
