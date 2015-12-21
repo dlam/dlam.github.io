0974de972b557dfa8c910f52df89ebc0824cf80d
Dustin Lam <mynameisdustinlam@gmail.com>
2015-12-21 00:48:56 -0500

    Add functionality to load a page on window.onLoad.

--- src/js/SiteNavigator.js
+++ src/js/SiteNavigator.js
@@ -7,7 +7,9 @@ var SiteNavigator = function(win, mainContainer) {
   this.win = win;
   this.mainContainer = mainContainer;
 
+  // TODO(dustin): Implement a DownloadTaskManager as a helper for all these XMLHttpRequests.
   // TODO(dustin): Put this in a better place.
+  // TODO(dustin): Use a kv-pair to map pages over an array to facilitate onLoad.
   this.navPages = [];
 
   this.navPages[0] = new Page('MENU', 'icon_menu', '/', function (mainContainer) {
@@ -41,7 +43,7 @@ var SiteNavigator = function(win, mainContainer) {
       }
     };
 
-    xmlHttp.open("GET", url, true);
+    xmlHttp.open('GET', url, true);
     xmlHttp.send();
   });
 
@@ -68,6 +70,7 @@ var SiteNavigator = function(win, mainContainer) {
   });
 };
 
+// TODO(dustin): It might be worth combining this into standard flow with callbacks.
 SiteNavigator.prototype.navigateTo = function(page) {
   this.win.history.pushState(null, 'Dustin Lam - ' + page.label, page.path);
   page.loadHandler(this.mainContainer);
@@ -77,5 +80,31 @@ SiteNavigator.prototype.navigateTo = function(page) {
 // SiteNavigator.prototype.goBack = function() {};
 // SiteNavigator.prototype.goForward = function() {};
 
-SiteNavigator.prototype.onLoad = function() {};
+SiteNavigator.prototype.onLoad = function() {
+  // TODO(dustin): We are gonna need chemo for this cancer.
+  var opts = this.win.location.search;
+  opts = opts.substring(1, opts.length - 1);
+  opts = opts.split('&');
+
+  var page;
+  switch(opts[0].split('=')[1]) {
+    case 'home':
+      page = this.navPages[1];
+      break;
+    case 'about':
+      page = this.navPages[2];
+      break;
+    case 'blog':
+      page = this.navPages[3];
+      break;
+    case 'projects':
+      page = this.navPages[4];
+      break;
+    default:
+      page = this.navPages[0];
+      break;
+  }
+
+  this.navigateTo(page);
+};
 
