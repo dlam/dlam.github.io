f0aef4d69a4b7bd4617c7f3ed0218cf21a3e1641
Dustin Lam <mynameisdustinlam@gmail.com>
2020-05-04 22:55:32 -0700

    Updates

--- backup/src/js/Page.js
+++
@@ -1,9 +0,0 @@
-/* exported Page */
-
-var Page = function(label, icon, path, loadHandler) {
-  this.label = label;
-  this.icon = icon;
-  this.path = path;
-  this.loadHandler = loadHandler;
-};
-
---
+++ precache-manifest.26c82471e7b15e4292628efb94215e87.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5abe32978a8015ac4802a7fefb420f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c965ae93b6a9eb6e539",
+    "url": "/static/css/main.54975b9c.chunk.css"
+  },
+  {
+    "revision": "059fbc3c1577b28d4704",
+    "url": "/static/js/2.e5a936f2.chunk.js"
+  },
+  {
+    "revision": "67935b6d863b3e23bc08cf31dbc40ecc",
+    "url": "/static/js/2.e5a936f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c965ae93b6a9eb6e539",
+    "url": "/static/js/main.340f9294.chunk.js"
+  },
+  {
+    "revision": "926a58a02423e859b245",
+    "url": "/static/js/runtime-main.399c3a9c.js"
+  },
+  {
+    "revision": "41089b2f99d519db70b323937dad7bab",
+    "url": "/static/media/account.41089b2f.svg"
+  },
+  {
+    "revision": "c30c213863edfb05f2a4af85f778e994",
+    "url": "/static/media/code-tags.c30c2138.svg"
+  },
+  {
+    "revision": "0c07127cfd4828956e73108b32efcd35",
+    "url": "/static/media/home.0c07127c.svg"
+  },
+  {
+    "revision": "0b4bec49ed29b1a7fdd4b17df62a1a5a",
+    "url": "/static/media/icon_github.0b4bec49.svg"
+  },
+  {
+    "revision": "1becc951e3ec714b3f9a6ade43084aba",
+    "url": "/static/media/icon_linkedin.1becc951.svg"
+  },
+  {
+    "revision": "98fbad94f2e6dc93b9c93829d674f5ae",
+    "url": "/static/media/icon_twitter.98fbad94.svg"
+  },
+  {
+    "revision": "fe6be2808f32956147c5c7bcfac8b820",
+    "url": "/static/media/menu.fe6be280.svg"
+  },
+  {
+    "revision": "7bb49de9afc7fdaf204e94e2851a2b71",
+    "url": "/static/media/pen.7bb49de9.svg"
+  },
+  {
+    "revision": "3480e592575d645569d0036cad5ba8ae",
+    "url": "/static/media/talk1.3480e592.png"
+  },
+  {
+    "revision": "c07dff779dccb9abc5bddb6827d1b1ba",
+    "url": "/static/media/talk2.c07dff77.png"
+  }
+]);
